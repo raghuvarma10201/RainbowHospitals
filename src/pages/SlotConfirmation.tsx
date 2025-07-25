ff95d111dfc45696cac03f4df5bd27b5c6d3bff4
rajeshanglar <51995360+rajeshanglar@users.noreply.github.com>
2025-07-25 14:51:54 +0530

    slot Confirmation UI
    slot Confirmation UI

--- src/pages/SlotConfirmation.tsx
+++ src/pages/SlotConfirmation.tsx
@@ -29,16 +29,16 @@ const SlotConfirmation: React.FC = ({route}: any) => {
             </View>
           </View>
           <View style={styles.doctorDetails}>
-            <Text style={[styles.docName, {color: '#4CC2BF'}]}>
+            <Text style={[styles.docName, {fontSize: 16, color: '#4CC2BF', fontFamily: 'ProximaNovaA-Semibold'}]}>
               {doctor?.name}
             </Text>
-            <Text style={[styles.docName, {fontSize: 12}]}>
+            <Text style={[styles.docName, {fontSize: 12, marginTop: 3,}]}>
               {doctor?.designation}
             </Text>
             <Text style={[styles.docName, {fontSize: 12}]}>
               {doctor?.speciality}
             </Text>
-            <Text style={[styles.docName, {fontSize: 14, color: '#4CC2BF'}]}>
+            <Text style={[styles.docName, {fontSize: 13, color: '#4CC2BF', marginTop: 3, marginBottom: 10}]}>
               {`Experience 15 Years`}
             </Text>
             <View style={styles.consultBtnsContainer}>
@@ -141,7 +141,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#3C2871',
     paddingTop: h * 0.1,
     paddingHorizontal: w * 0.02,
-    width: w * 0.8,
+    width:'90%',
     alignSelf: 'center',
     marginTop: h * 0.12,
     borderTopLeftRadius: w * 0.1,
@@ -192,12 +192,14 @@ const styles = StyleSheet.create({
   docName: {
     fontSize: 20,
     color: '#fff',
+    fontFamily: 'ProximaNovaA-Regular',
   },
   consultBtnsContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     marginVertical: h * 0.01,
+    marginBottom: 15,
   },
   consultBtn: {
     paddingVertical: w * 0.03,
@@ -207,16 +209,18 @@ const styles = StyleSheet.create({
     width: '48%',
   },
   consultBtnTxt: {
-    fontSize: 12,
+    fontSize: 11,
     color: '#000',
-    textAlign: 'right',
+    textAlign: 'left',
+    fontFamily: 'ProximaNovaA-Regular',
+    paddingLeft: 32,
   },
   iconContainer: {
-    height: '300%',
-    width: '30%',
+    height:Dimensions.get('window').height * 0.08,
+    width: 30,
     position: 'absolute',
     backgroundColor: '#4CC2BF',
-    left: w * 0.01,
+    left: 5,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -233,7 +237,7 @@ const styles = StyleSheet.create({
   },
   calenderContainer: {
     backgroundColor: '#fff',
-    width: w * 0.8,
+    width:'90%',
     alignSelf: 'center',
     paddingBottom: h * 0.03,
   },
@@ -254,10 +258,13 @@ const styles = StyleSheet.create({
     color: '#000',
   },
   flexSub: {
-    fontSize: 14,
+    fontSize: 12,
+    paddingHorizontal: 10,
     color: '#000',
-    backgroundColor: '#4CC2BF',
-    paddingHorizontal: w * 0.01,
+    backgroundColor: '#b1e2e1ff',
+    paddingVertical: 5,
+    borderRadius:3,
+    marginBottom: 5,
   },
   paymentBlock: {
     paddingVertical: h * 0.01,
