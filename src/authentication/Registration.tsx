6619e6129260c8d08a8b90dbed36a69dd8b63206
rajeshanglar <51995360+rajeshanglar@users.noreply.github.com>
2025-07-31 18:07:34 +0530

    registrations
    registrations

--- src/authentication/Registration.tsx
+++ src/authentication/Registration.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, ImageBackground, ScrollView, TextInput, Image, TouchableOpacity, Dimensions } from 'react-native';
-import { Button, Text, Checkbox } from 'react-native-paper';
+import { StyleSheet, View, ImageBackground, ScrollView, TextInput, Image, TouchableOpacity,  Pressable, Platform } from 'react-native';
+import { Button, Text, } from 'react-native-paper';
 import { Dropdown } from 'react-native-element-dropdown';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
@@ -13,33 +13,66 @@ import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { AuthStackParamList } from '../navigation/types';
 import { useAuth } from '../context/AuthContext';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
-const local_data = [
+const gender_data = [
   {
     value: '1',
-    lable: '+91',
+    lable: 'male',
 
   },
   {
     value: '2',
-    lable: '+92',
+    lable: 'Female',
 
   },
 ];
 
+const countries =[
+  { label: 'India', value: 'IN' },
+  { label: 'United States', value: 'US' },
+  { label: 'United Kingdom', value: 'UK' },
+  { label: 'Canada', value: 'CA' },
+  { label: 'Australia', value: 'AU' },
+];
+
 const LoginSchema = Yup.object({
-  mobileNumber: Yup.string().required('Please enter valid mobile number')
+  mobileNumber: Yup.string().required('Please enter valid mobile number'),
+  dob: Yup.string().required('Date of Birth is required'),
 });
 const Registration: React.FC = () => {
+  const [checked, setChecked] = useState(false);
+
+ // DateTimePickerModal
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const showDatePicker = () => {setDatePickerVisibility(true);};
+  const hideDatePicker = () => {setDatePickerVisibility(false);};
+  const handleConfirm = (date: Date) => {
+    setSelectedDate(date);
+    hideDatePicker();
+  };
+ // DateTimePickerModal End
+
+
+
+
+
+
   type NavigationProp = NativeStackNavigationProp<AuthStackParamList, 'Login'>;
   const navigation = useNavigation<NavigationProp>();
-  const [value, setValue] = useState('');
   const [country, setCountry] = useState('1');
+  const [gender, setGender] = useState('1');
   const { setLoggedIn } = useAuth();
   const [loading, setLoading] = useState(false);
 
   const formik = useFormik({
-    initialValues: { mobileNumber: '' },
+
+    initialValues: {
+       mobileNumber: '',
+       dob: '',
+      
+      },
     validationSchema: LoginSchema,
     onSubmit: async (values, { setSubmitting, setErrors, setFieldError }) => {
       setLoading(true);
@@ -50,6 +83,8 @@ const Registration: React.FC = () => {
         });
         if (response.status == 200 && response.success == true) {
           await AsyncStorage.multiSet([['mobileNumber', values.mobileNumber]]);
+ 
+          
           ToastService.success('Success', 'Otp sent successfully');
           navigation.navigate('Otp');
         }
@@ -61,6 +96,7 @@ const Registration: React.FC = () => {
         // Basic error surface – adapt as needed
         ToastService.error('Invalid credentials', 'Please try again');
         setErrors({ mobileNumber: 'Invalid credentials' });
+        setErrors({ mobileNumber: 'Invalid credentials' });
       } finally {
         setSubmitting(false);
         setLoading(false);
@@ -70,17 +106,139 @@ const Registration: React.FC = () => {
   });
 
   return (
-    <ScrollView style={styles.container}>
-      <View>
+    <ScrollView contentContainerStyle={styles.scrollContent}>
+      <View style={styles.container}>
+
+      <Text variant="headlineMedium" style={styles.title}>Registration</Text>
+      <Text style={styles.labelText}>You need to register your account only once.</Text>
         
+        <View style={styles.regForm}>
 
+        <View style={styles.formRow}>
+           <Text style={styles.formLabel}>Title *</Text>
+            <TextInput style={styles.formInput}
+              keyboardType="numeric"          // shows numeric keyboard
+              maxLength={10}                  
+              placeholder="Enter Title"
+              onChangeText={formik.handleChange('Title')}
+              onBlur={formik.handleBlur('mobileNumber')}
+              value={formik.values.mobileNumber}
+            />
+        </View>
 
+        <View style={styles.formRow}>
+           <Text style={styles.formLabel}>First Name *</Text>
+            <TextInput style={styles.formInput}
+             keyboardType="default"                  
+             placeholder="Enter First Name"
+              placeholderTextColor="#000"
+             onChangeText={formik.handleChange('firstName')}
+             onBlur={formik.handleBlur('firstName')}
+                //  value={formik.values.firstName}
+             />
+        </View>
 
+        <View style={styles.formRow}>
+           <Text style={styles.formLabel}>Last Name *</Text>
+            <TextInput style={styles.formInput}
+             keyboardType="default"                  
+             placeholder="Enter Last Name"
+              placeholderTextColor="#000"
+             onChangeText={formik.handleChange('lastName')}
+             onBlur={formik.handleBlur('lastName')}
+       
+                //  value={formik.values.firstName}
+           />
+        </View>
 
-        <View style={styles.loginForm}>
-          <Text variant="headlineMedium" style={styles.title}>SIGN IN</Text>
-          <Text style={styles.labelText}>Mobile Number</Text>
-          <View style={styles.formViewGroup}>
+        <View style={styles.formRow}>
+           <Text style={styles.formLabel}>Email *</Text>
+            <TextInput style={styles.formInput}
+             keyboardType="email-address"
+             autoCapitalize="none"
+             placeholder="Enter your email"
+              placeholderTextColor="#000"
+             onChangeText={formik.handleChange('email')}
+             onBlur={formik.handleBlur('email')}
+            //  value={formik.values.email}
+             />
+        </View>
+
+        <View style={styles.formRow}>
+           <Text style={styles.formLabel}>Country</Text>
+           <Dropdown
+              style={styles.dropdownSelect}
+              selectedTextStyle={styles.selectedTextContry}
+              placeholderStyle={styles.placeholderCountry}
+              maxHeight={200}
+              value={country}
+              data={countries}
+              valueField="value"
+              labelField="lable"
+              placeholder="Select country"
+              containerStyle={styles.dropdownList}
+              activeColor="#000"
+              onChange={e => setCountry(e.value)}
+            />       
+        </View>
+
+        <View style={styles.formRow}>
+           <Text style={styles.formLabel}>Date of Birth</Text>
+              <TouchableOpacity onPress={showDatePicker}>
+                <Text style={styles.formInput}>
+                  {selectedDate ? selectedDate.toDateString() : 'Select Date'}
+                </Text>
+              </TouchableOpacity>
+              <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={handleConfirm}
+                onCancel={hideDatePicker}
+              />    
+        </View>
+
+        <View style={styles.formRow}>
+           <Text style={styles.formLabel}>Gender *</Text>
+           <Dropdown
+              style={styles.dropdownSelect}
+              selectedTextStyle={styles.selectedTextContry}
+              placeholderStyle={styles.placeholderCountry}
+              maxHeight={200}
+              value={gender}
+              data={gender_data}
+              valueField="value"
+              labelField="lable"
+              placeholder="Select Gender"
+              containerStyle={styles.dropdownList}
+              activeColor="#000"
+              onChange={e => setGender(e.value)}
+            />
+        </View>
+
+        <View style={styles.formRow}>
+           <Text style={styles.formLabel}>Address</Text>
+            <TextInput style={styles.formInput} />
+        </View>
+
+        <View style={styles.formRow}>
+           <Text style={styles.formLabel}>Pincode</Text>
+            <TextInput style={[styles.formInput, {color:'#000',}]} />
+        </View>
+
+        <View style={styles.formRow}>
+          <TouchableOpacity
+            style={styles.checkboxContainer}
+            onPress={() => setChecked(!checked)}
+          >
+            <View style={[styles.checkbox, checked && styles.checked]}>
+              {checked && <Text style={styles.checkmark}>✓</Text>}
+            </View>
+            <Text style={styles.checkBoxlabel}>Agree to our Terms of Services and Privacy Policy</Text>
+          </TouchableOpacity>
+        </View>
+
+ 
+          {/* <View style={styles.formViewGroup}>
             <Dropdown
               style={styles.dropdownSelect}
               selectedTextStyle={styles.selectedTextContry}
@@ -105,7 +263,7 @@ const Registration: React.FC = () => {
               onBlur={formik.handleBlur('mobileNumber')}
               value={formik.values.mobileNumber}
             />
-          </View>
+          </View> */}
           {formik.touched.mobileNumber && formik.errors.mobileNumber && <Text style={styles.errorMessage}>{formik.errors.mobileNumber}</Text>}
           <TouchableOpacity style={styles.primaryBt} onPress={() => formik.handleSubmit()}>
             <Text style={styles.primaryBtText}>Get OTP</Text>
@@ -122,28 +280,59 @@ const Registration: React.FC = () => {
 export default Registration;
 
 const styles = StyleSheet.create({
+
+
+
+
+
+  scrollContent: {
+    padding: 0,
+    paddingBottom: 100,
+  },
+
   container: {
     flex: 1,
+    paddingHorizontal:15,
     paddingBottom: 10,
     paddingTop: 0,
   },
-  logoImage: {
-    marginHorizontal: 'auto',
-    marginTop: '7%',
-    marginBottom: '8%',
-    width: '100%',
-    height: Dimensions.get('window').height * 0.11,
-    justifyContent: 'space-between',
-  },
+
   title: {
-    color: '#00B3AE', fontSize: 20, fontWeight: 'normal', marginTop: -12,
-    textAlign: 'center', textTransform: 'uppercase', marginBottom: 5, fontFamily: 'ProximaNovaA-Bold'
+    color: '#00B3AE', 
+    fontSize: 20, 
+    fontWeight: 'normal',   
+    textAlign: 'center', 
+    textTransform: 'uppercase', 
+    fontFamily: 'ProximaNovaA-Bold',
+    marginTop:60,
   },
 
   labelText: {
-    fontSize: 14, fontWeight: 'normal', color: '#000', marginBottom: 10, fontFamily: 'ProximaNovaA-Regular',
+    fontSize: 14, 
+    fontWeight: 'normal', 
+    color: '#000', 
+    marginBottom: 10, 
+    fontFamily: 'ProximaNovaA-Regular',
     textAlign: 'center',
   },
+
+  primaryBt: {
+    borderRadius: 40,
+    backgroundColor: '#818385',
+    marginBottom: 20,
+    padding: 10,
+    width: 200,
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+  primaryBtText: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 'normal',
+    textAlign: 'center',
+
+  },
+
   errorMessage: {
     color: '#FFACAC',
     marginTop: 0,
@@ -155,61 +344,102 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20
 
   },
-  formViewGroup: {
-    flexDirection: 'row',
-    overflow: 'hidden',
-    alignItems: 'center',
-    marginBottom: 5,
-    borderRadius: 0,
-    paddingStart: 10,
-    paddingEnd: 10,
-    paddingTop: 0,
-    paddingBottom: 10,
-    backgroundColor: '#E6E7E8',
-  },
-  formInput: {
-    height: 32,
-    fontSize: 16,
-    paddingHorizontal: 10,
-    paddingVertical: 0,
-    marginTop: 10,
+
+
+
+
+
+
+
+  formRow:{marginBottom:12,},
+  formLabel:{fontSize:12, fontFamily:'ProximaNovaA-Regular', color:'#000', marginBottom:5,},
+  formInput:{
+    height:40,
+    flex:1,
+    borderWidth:1,
+    borderColor:'#C7E8E7',
+    borderRadius:10,
+    padding:10,
+    backgroundColor:'#C7E8E7',
     fontFamily:'ProximaNovaA-Regular',
+    fontSize:14,
+    color:'#000',
   },
-  primaryBt: {
-    borderRadius: 40,
-    backgroundColor: '#818385',
-    marginBottom: 20,
-    padding: 10,
-    width: 200,
-    alignSelf: 'center',
+  
+
+  formButton:{
+    backgroundColor:'#3C2871',
+    borderRadius:10,
+    padding:10,
     marginTop:10,
   },
-  primaryBtText: {
-    color: '#fff',
-    fontSize: 14,
-    fontWeight: 'normal',
-    textAlign: 'center',
-
+  formButtonText:{
+    color:'#fff',
+    textAlign:'center',
+    fontSize:14,
+    fontFamily:'ProximaNovaA-Bold',
+    fontWeight:'bold',
+    padding:5,
+    borderRadius:10, 
   },
 
+  regForm:{marginTop:10,},
+
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    paddingVertical: 8,
+  },
+  checkBoxlabel:{
+    fontFamily:'ProximaNovaA-Regular',
+    fontSize:13,
+    flexWrap: 'wrap',
+    paddingRight:20,
+  
+  },
+
+  checkbox: {
+    width: 26,
+    height: 26,
+    borderWidth: 2,
+    borderColor: '#444',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 8,
+    borderRadius: 4,
+  },
+  checked: {
+    backgroundColor: '#3C2871',
+    borderColor: '#3C2871',
+  },
+  checkmark: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+
+  //--
+  
   dropdownSelect: {
-    height: 30,
-    paddingHorizontal: 10,
-    marginTop: 10,
-    borderRightWidth: 2,
-    borderRightColor: '#6D6F71',
-    width: 80,
+    paddingHorizontal: 10, 
+    width: '100%',
+    height:40,
+    flex:1,
+    borderWidth:1,
+    borderColor:'#C7E8E7',
+    borderRadius:10,
+    backgroundColor:'#C7E8E7',
   },
 
   placeholderCountry: {
-    fontSize: 16,
+    fontSize: 14,
     color: '#000',
-    fontFamily:'ProximaNovaA-Regular',
+    fontFamily: 'ProximaNovaA-Regular',
   },
   selectedTextContry: {
     fontSize: 16,
     color: '#000',
-    fontFamily:'ProximaNovaA-Regular',
+    fontFamily: 'ProximaNovaA-Regular',
   },
 
   dropdownList: {
@@ -217,55 +447,8 @@ const styles = StyleSheet.create({
     marginRight: 10,
     padding: 0,
     textAlign: 'left',
-    fontFamily:'ProximaNovaA-Regular',
-  },
-
-  imgTextGroup: {
-    paddingLeft: 20,
-  },
-  imgTextBox: {
-    width: '55%',
-    paddingTop: 10,
-
-    paddingBottom: 10,
-    paddingLeft: 15,
-    paddingRight: 10,
-    backgroundColor: '#3C2871',
-    borderRadius: 10,
-    marginTop: 0,
-
+    fontFamily: 'ProximaNovaA-Regular',
+   backgroundColor:'#000',
 
   },
-  textbeforeDot: { position: 'relative' },
-  imgTextTitle: {
-    fontSize: Dimensions.get('window').height * 0.024,
-    fontWeight: 'normal',
-    color: '#fff',
-    textAlign: 'left',
-    paddingBottom: 20,
-  },
-
-  beforeDot: {
-    position: 'absolute',
-    top: '40%',
-    right: '-20%',
-    width: 30,
-    height: 30,
-    backgroundColor: '#00B3AE',
-    borderRadius: 50,
-    borderWidth: 7,
-    borderColor: '#fff',
-    marginRight: 6,
-
-  },
-  loginImg: {
-    height: Dimensions.get('window').height * 0.43,
-    width: '100%',
-    marginTop: '-36%',
-    position: 'relative',
-    alignSelf: 'flex-end',
-    marginLeft: 'auto',
-    right: '-15%',
-  }
-
 });
