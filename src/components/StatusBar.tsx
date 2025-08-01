3a666fcf34b7bcc6ff27b752545cac7dfee0518c
Aitha Nishanth Raghava <naitha@ctewebapps.com>
2025-08-01 18:05:07 +0530

    Statusbar changed

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,11 +1,37 @@
-import { SafeAreaView, StatusBar, View } from "react-native";
+// src/components/StatusBar.tsx
+import React from 'react';
+import {
+  StatusBar,
+  Platform,
+  View,
+  StyleSheet,
+  StatusBarStyle,
+} from 'react-native';
 
-const MyStatusBar = ({backgroundColor, ...props}: any) => (
-  <View style={{ backgroundColor }}>
-    <SafeAreaView>
-      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-    </SafeAreaView>
-  </View>
-);
+interface MyStatusBarProps {
+  backgroundColor: string;
+  barStyle?: StatusBarStyle;
+}
+
+const MyStatusBar: React.FC<MyStatusBarProps> = ({
+  backgroundColor,
+  barStyle = 'light-content',
+}) => {
+  return (
+    <View style={[styles.statusBar, { backgroundColor }]}>
+      <StatusBar
+        translucent
+        backgroundColor={backgroundColor}
+        barStyle={barStyle}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  statusBar: {
+    height: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+});
 
 export default MyStatusBar;
