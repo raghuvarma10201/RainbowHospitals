1b05c60ec34fe6b61d76fd3b45a9d717e4052a3f
Sunny-exwyn <sunny.b@exwyn.com>
2025-08-04 16:34:05 +0530

    Fast Image
    Added react native fast image npm for fast loading of image urls

--- android/app/src/main/java/com/rainbowhospitals/MainApplication.kt
+++ android/app/src/main/java/com/rainbowhospitals/MainApplication.kt
@@ -11,7 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 import com.oney.WebRTCModule.WebRTCModuleOptions
-import com.rainbowhospitals.ScreenSharePackage
+// import com.rainbowhospitals.ScreenSharePackage
 
 
 class MainApplication : Application(), ReactApplication {
@@ -22,7 +22,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
-              add(ScreenSharePackage())
+              // add(ScreenSharePackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
